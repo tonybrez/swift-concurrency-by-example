35ea0dbf230d98b6fbff77be5196f2ac83596c12
Anthony Benitez <anthony.benitezrodriguez@gmail.com>
2023-07-23 14:24:44 -0500

    Initial Commit

---
+++ swift-concurrency-by-example/swift_concurrency_by_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_concurrency_by_exampleApp.swift
+//  swift-concurrency-by-example
+//
+//  Created by Anthony Benitez-Rodriguez on 7/23/23.
+//
+
+import SwiftUI
+
+@main
+struct swift_concurrency_by_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift-concurrency-by-exampleUITests/swift_concurrency_by_exampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  swift_concurrency_by_exampleUITestsLaunchTests.swift
+//  swift-concurrency-by-exampleUITests
+//
+//  Created by Anthony Benitez-Rodriguez on 7/23/23.
+//
+
+import XCTest
+
+final class swift_concurrency_by_exampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
